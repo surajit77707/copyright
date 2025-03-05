159bf6cfe8cd917b7e33d4f94fa4f429f19183aa
surajit77707 <surajitsingh77707@gmail.com>
2025-03-05 00:02:23 -0800

    Update cp.py

--- cp.py
+++ cp.py
@@ -19,7 +19,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 API_ID = 23967991
 API_HASH = "a2c3ccfaff4c2dbbff7d54981828d4f1"
-BOT_TOKEN = "8151352194:AAHdg9XIwWB4ABP3GjpmBCTKG7rmsdLiOA8"
+BOT_TOKEN = "8151352194:AAFZFL1TO2Iuht3W4hoSTl2T3Xk3Y4jCHdA"
 DEVS = [1883889098, 7638575366]
 BOT_USERNAME = "banxeditbot" # change your bot username without 
 OWNER_ID = 7638575366
